b32f3d0eda7b7d50212c3531b055125072aad040
rodrigo_bojorquez <rbojorquez1620@gmail.com>
2024-04-05 19:31:03 -0500

    cambios en PatientTabs, en estilos de los tabs

--- src/tabs/PatientTabs.jsx
+++ src/tabs/PatientTabs.jsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import LogOutButton from '../components/LogOutButton';
+import { Entypo } from '@expo/vector-icons';
 
 // SCREENS
 import HomePatient from "../screens/patient/HomePatient";
@@ -12,9 +13,18 @@ const PatientTabs = () => {
   return (
     <Tab.Navigator
       initialRouteName='Home'
-      screenOptions={{
-        headerRight: (props) => <LogOutButton />
-      }}
+
+      // TABS ICONS 
+
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ foucused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            return <Entypo name='home' size={25} color="black" />
+          }
+        },
+      })}
     >
       <Tab.Screen name='Home' component={HomePatient} />
 
