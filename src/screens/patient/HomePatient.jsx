f75f36982e307ffac9d94812535f5f9c9a42e722
DarkShadowXx64 <gabo.garcia.osorio@gmail.com>
2024-04-07 10:17:32 -0500

    pene

--- src/screens/patient/HomePatient.jsx
+++ src/screens/patient/HomePatient.jsx
@@ -10,6 +10,7 @@ import { useAppStore } from '../../stores/appStore';
 
 const HomePatient = () => {
     const [search, setSearch] = React.useState('');
+    const [userInfo, setUserInfo] = useState(null);
     const [appointments, setAppointments] = useState([]);
     const profileData = useAppStore(state => state.profileData);
 
@@ -17,6 +18,30 @@ const HomePatient = () => {
         setSearch(text);
     };
 
+    useEffect(() => {
+        const getUserInfo = async () => {
+            try {
+                const token = profileData.token;
+    
+                if (!token) {
+                    throw new Error('Token de autenticación no encontrado');
+                }
+    
+                const response = await axiosClient.get(`/user/me`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                setUserInfo(response.data); // Actualiza el estado de userInfo con la información del perfil
+            } catch (error) {
+                console.error('Error fetching user profile:', error);
+            }
+        };
+    
+        getUserInfo();
+    }, []);
+    
+    
     useEffect(() => {
         const getAppointmentsData = async () => {
             try {
@@ -33,15 +58,13 @@ const HomePatient = () => {
     }, [])
     
     return (
-        <ScrollView style={tw`bg-white pt-12 h-full`}>
+        <ScrollView style={tw`bg-white pt-8 h-full`}>
             <View style={tw`items-center bg-white`}>
                 <View style={tw`flex flex-row items-center bg-white justify-around`}>
-                    {/* <Image source={rodri} style={tw`w-24 h-24 mr-4`} /> */}
-                    <View>
+                    <View style={tw`flex  items-center`}>
                         <Text style={tw`text-2xl font-bold text-[#858585]`}>Bienvenido,</Text>
-                        <Text style={tw`text-2xl font-bold text-black mt-1`}>Rodrigo Bojorquez</Text>
+                        <Text style={tw`text-2xl font-bold`}>{userInfo ? userInfo.username : 'Nombre de Usuario'}</Text>
                     </View>
-                    <Image source={OneNotifi} />
                 </View>
                 <View style={tw`flex flex-row mx-4 items-center mt-4 px-4 py-4 bg-gray-200 rounded-lg`}>
                     <Image source={Buscar} style={tw`mr-3`} />
